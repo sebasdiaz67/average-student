c840179b0259c278862ed2c9b1e27f7a04bb08cf
Sebastian Rosero <sebasdiaz67@gmail.com>
2023-12-30 00:46:26 -0500

    average student in js

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # average-student
+Lógica de Programación
+
+Título:
+Ejercicio2: Calculadora de Promedio de Notas con Validación y Mensajes Personalizados, en JavaScript.
+
+Objetivo:
+Desarrollar un programa en JavaScript que calcule el promedio de tres notas ingresadas por el usuario, verificando su validez en un rango de 0 a 10, y proporcionando mensajes personalizados dependiendo del resultado del promedio, utilizando estructuras de decisión y herramientas como Visual Studio Code y GitHub.
+
+Paso 1: Crear el archivo JavaScript en Visual Studio Code
+
+Abre Visual Studio Code, crea un nuevo archivo y guárdalo con el nombre `calculadoraPromedio.js`.
+
+Paso 2: Desarrollar el código en JavaScript
+
+Utiliza JavaScript para realizar las operaciones necesarias para calcular el promedio de notas, verificar el rango válido de las notas y mostrar el mensaje correspondiente según el resultado, vamos a tomar en cuenta las instrucciones del ejercicio 1, del cual se realizó diagrama de flujo.
+Desarrolla un programa que calcule el promedio de tres notas ingresadas por el usuario, solicitando también el nombre del alumno y la materia. Además, el programa debe verificar si las notas ingresadas se encuentran dentro del rango válido (por ejemplo, de 0 a 10). Una vez calculado el promedio de las tres notas, el programa debe evaluar si dicho promedio es mayor o igual a 7.
+
+Si el promedio es mayor o igual a 7, el programa imprimirá: 'Nombre del alumno, ¡felicidades! Has aprobado con un promedio de [indicar promedio].' En caso de que el promedio sea menor a 7, el programa imprimirá: 'Nombre del alumno, gracias por tu esfuerzo. Nos vemos pronto en clase. El promedio obtenido es [indicar promedio].'
+
+Paso 3: Configurar Git y subir el código a GitHub
+Agregar el código al repositorio
+    • Copia tu archivo calculadoraPromedio.js al directorio clonado del repositorio en tu máquina local.
+    • Abre la terminal en la carpeta del repositorio clonado.
+    • Añade los cambios al área de preparación de Git y realiza un commit con los cambios:
+git add calculadoraPromedio.js
+git commit -m "Añadir código de la calculadora de promedio"
+
+    • Sube los cambios al repositorio remoto en GitHub:
+bash
+Copy code
+git push origin main
+
+Paso 4: Enviar la liga del repositorio público al correo especificado
+
+Envía la URL del repositorio público que contiene el código a la dirección de correo tutoria@commercemind.education, asunto del correo: Lógica de programación-Ejercicio2
---
+++ average-student.js
@@ -0,0 +1,76 @@
+// grades
+document.getElementById('grade1').innerHTML = '0.0';
+document.getElementById('grade2').innerHTML = '0.0';
+document.getElementById('grade3').innerHTML = '0.0';
+// average
+document.getElementById('average').innerHTML = '';
+
+// enter the name
+function insertStudent() {
+    let student = prompt('Enter the student name:');
+    if (student != null) {
+        if (!isNaN(student)) {
+            alert('Please insert a valid name...')
+        } else {
+            document.getElementById('nameStudent').innerHTML = student.toLocaleUpperCase();
+        }
+    }
+}
+
+// enter the grades
+function insertGrades() {
+    // firts validate the student name
+    let nameInput = document.getElementById('nameStudent');
+    if (nameInput.textContent == '') {
+        alert('Insert the student name');
+    } else {
+        // add the 3 grades
+        for (let i = 1; i <= 3; i++) {
+            let grade = prompt('Enter the grade' + i + ':');
+            if (grade == '') {
+                alert('Please insert the grade...');
+                clean();
+                return;
+            } else {
+                if (isNaN(grade)) {
+                    alert('Please insert a valid grade...');
+                    clean();
+                    return;
+                }
+                if (grade >= 0 && grade <= 10) {
+                    document.getElementById('grade' + i).innerHTML = Number(grade).toFixed(2);
+                } else {
+                    alert('Please insert grades between 0 and 10');
+                    clean();
+                    return;
+                }
+            }
+        }
+        // calculate the average
+        let grade1 = document.getElementById('grade1');
+        let grade2 = document.getElementById('grade2');
+        let grade3 = document.getElementById('grade3');
+        if (grade1.textContent != '' && grade2.textContent != '' && grade3.textContent != '') {
+            let average = (Number(grade1.textContent) + Number(grade2.textContent) + Number(grade3.textContent)) / 3;
+            let message = '';
+            if (average >= 7) {
+                message = nameInput.textContent.toLocaleUpperCase() +
+                    ', congratulations! You have passed with an average of ' + average.toFixed(2) + '.';
+            } else {
+                message = nameInput.textContent.toLocaleUpperCase() +
+                    ', thank you for your effort. See you soon in class. The average obtained is ' + average.toFixed(2) + '.';
+            }
+            document.getElementById('average').innerHTML = message;
+        }
+    }
+}
+
+// clean de data
+function clean() {
+    // grades
+    document.getElementById('grade1').innerHTML = '0.0';
+    document.getElementById('grade2').innerHTML = '0.0';
+    document.getElementById('grade3').innerHTML = '0.0';
+    // average
+    document.getElementById('average').innerHTML = '';
+}
